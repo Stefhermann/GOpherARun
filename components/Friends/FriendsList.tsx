db00f554c4abeb67f0a80c0fbe9bb0b6bc832cab
Xander Hill <xander.k.hill@gmail.com>
2025-04-21 16:55:14 -0500

    friends list (incomplete)

---
+++ components/Friends/FriendsList.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { useState } from "react";
+import { getFriendsList } from "@/app/friends/actions";
+
+// export function FriendsList({ userId }: { userId: string }) {
+//     const [friends, setFriends] 
+// }
